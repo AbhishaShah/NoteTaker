814b7ef2d518d2eb98aa6ddb41df03150758511f
abhisha shah <abhisha5993@gmail.com>
2019-12-31 11:07:39 -0500

    minor changes to learn build on heroku

--- server.js
+++ server.js
@@ -25,7 +25,8 @@ app.use('/users',usersRouter);
 app.use('/notes',notesRouter);
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+   
+    app.use(express.static(path.join(__dirname, "client", "build")))
 
     app.get('*',(req, res) => {
         res.sendFile(path.join(__dirname,'client','build','index.html'));
